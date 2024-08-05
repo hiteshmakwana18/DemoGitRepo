562a129c2b9c01278611dd0dfab53f76501447eb
Hitesh <hitesh.ddu@gmail.com>
2024-08-05 13:17:32 +0530

    my first change in java Demo444.java

---
+++ Demo444.java
@@ -0,0 +1,26 @@
+
+
+//import tools.Calc;
+//import tools.AdvCalc;
+//import tools.*;
+//import other.tools.*;
+//import.other.*;
+
+//import java.util.ArrayList;
+//import java.lang.*;
+
+import others.tools.calc;
+
+public class Demo{
+	public static void main(String args[])
+	{
+//		ArrayLis list=new ArrayList();
+		
+		calc obj=new calc();
+		AdvCalc obj1=new AdvCalc();
+		
+		//A obj2=new A();
+		
+		System.out.println();
+	}
+}
